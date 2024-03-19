615e9118650d99f0d7ff4b018eb7352264c85e40
Anders Wiklund <126193526+ycc140@users.noreply.github.com>
2024-03-19 11:52:32 +0100

    Simplified process body type hint choice.

--- src/api/documentation.py
+++ src/api/documentation.py
@@ -7,11 +7,17 @@ VERSION INFO::
 
     $Repo: fastapi_https
   $Author: Anders Wiklund
-    $Date: 2024-03-18 15:47:18
-     $Rev: 4
+    $Date: 2024-03-19 11:52:28
+     $Rev: 5
 """
 
-process_request_body_example = {
+# BUILTIN modules
+from typing import Annotated
+
+# Third party modules
+from fastapi import Body
+
+process_body_example = {
     "ok": {
         "summary": "success",
         "value": {"status": "SUCCESS"}},
@@ -20,3 +26,9 @@ process_request_body_example = {
         "value": {"status": "FAILURE"}}
 }
 """ OpenAPI process request body choice values. """
+
+ProcessTypeBodyChoice = Annotated[
+    dict,
+    Body(openapi_examples=process_body_example)
+]
+""" Process payload type hint with OpenAPI documentation. """
--- src/api/process_routes.py
+++ src/api/process_routes.py
@@ -7,21 +7,20 @@ VERSION INFO::
 
     $Repo: fastapi_https
   $Author: Anders Wiklund
-    $Date: 2024-03-18 15:47:18
-     $Rev: 4
+    $Date: 2024-03-19 11:52:28
+     $Rev: 5
 """
 
 # BUILTIN modules
 import asyncio
 from uuid import uuid4
-from typing import Annotated
 
 # Third party modules
-from fastapi import Depends, APIRouter, Body, HTTPException
+from fastapi import Depends, APIRouter, HTTPException
 
 # local modules
+from .documentation import ProcessTypeBodyChoice
 from ..tools.security import validate_authentication
-from .documentation import process_request_body_example
 from .models import ProcessResponseModel, ProcessingError
 
 # Constants
@@ -42,10 +41,7 @@ async def _fake_processing(payload: dict) -> bool:
 @ROUTER.post('', response_model=ProcessResponseModel,
              responses={500: {"model": ProcessingError}},
              dependencies=[Depends(validate_authentication)])
-async def process_payload(payload: Annotated[
-    dict,
-    Body(openapi_examples=process_request_body_example)
-]) -> ProcessResponseModel:
+async def process_payload(payload: ProcessTypeBodyChoice) -> ProcessResponseModel:
     """**Payload processing.**"""
 
     if await _fake_processing(payload):
